e48a9d5e8c47fae8a6810a8d4b19666317f8d2de
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2019-03-12 12:56:45 +0100

    Use safe_load for yaml files

--- pyropod/ropod/utils/logging/config.py
+++ pyropod/ropod/utils/logging/config.py
@@ -9,7 +9,7 @@ def config_logger(config_file=None, filename=None):
         config_file = yaml.load(pkg_resources.resource_filename('ropod.pyre_communicator', 'config/logging.yaml'))
 
     with open(config_file) as yaml_file:
-        log_config = yaml.load(yaml_file)
+        log_config = yaml.safe_load(yaml_file)
 
     if filename:
         log_config['handlers']['file']['filename'] = '/var/log/ropod/fms/%s.log' % filename
