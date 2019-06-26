59a92a88ccc3744ce39b1a3f84f7bf529c2665dd
Argentina Ortega Sáinz <argentina.ortega@h-brs.de>
2019-06-26 15:28:49 +0200

    pyropod/utils/config: Split yaml processing from file being read
    This is being used by the FMS when reading the config file
    using importlib_resources and just processing the yaml file
    using this utils

--- pyropod/ropod/utils/config.py
+++ pyropod/ropod/utils/config.py
@@ -3,6 +3,11 @@ import yaml
 
 def read_yaml_file(config_file_name):
     file_handle = open(config_file_name, 'r')
-    data = yaml.safe_load(file_handle)
+    data = get_config(file_handle)
     file_handle.close()
+    return data
+
+
+def get_config(yaml_file):
+    data = yaml.safe_load(yaml_file)
     return data
